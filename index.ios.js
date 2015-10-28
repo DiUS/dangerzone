2085cb2bdbdc5117d7c14560c4f5745bf421a2ba
Sonny Mai <smai@dius.com.au>
2015-10-29 10:32:29 +1100

    Intiial commit

---
+++ index.ios.js
@@ -0,0 +1,52 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
+'use strict';
+
+var React = require('react-native');
+var {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+} = React;
+
+var dangerzone = React.createClass({
+  render: function() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to the Dangerzone
+        </Text>
+        <Text style={styles.instructions}>
+          Lets get dangerous
+        </Text>
+        <Text style={styles.instructions}>
+          =)
+        </Text>
+      </View>
+    );
+  }
+});
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('dangerzone', () => dangerzone);
